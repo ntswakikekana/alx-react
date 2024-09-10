645b24f889e8ff98bda96b00f71a5c6ae34f3cb5
ntswakikekana46 <ntswakikekana46@gmail.com>
2024-09-10 03:49:44 -0700

    webapck success

--- 0x01-react_intro/task_0/dashboard/src/App.js
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -1,21 +1,20 @@
-import React from 'react';
 import './App.css';
-import logo from './holberton_logo.jpg';
+import holberton_logo from './holberton-logo.jpg';
 
 function App() {
   return (
-    <div>
-      <header className="App-header">
+    <>
+      <div className="App-header">
         <img src={holberton_logo} alt="logo" />
         <h1>School dashboard</h1>
-      </header>
+      </div>
       <div className="App-body">
         <p>Login to access the full dashboard</p>
       </div>
-      <footer className="App-footer">
+      <div className="App-footer">
         <p>Copyright 2020 - Holberton School</p>
-      </footer>
-    </div>
+      </div>
+    </>
   );
 }
 
