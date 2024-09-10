777c3efc1193dd261953d3de59211f9ed5ecf5e3
ntswakikekana46 <ntswakikekana46@gmail.com>
2024-09-10 06:07:30 -0700

    debug

--- 0x01-react_intro/task_3/dashboard/src/setupTests.js
+++ 0x01-react_intro/task_3/dashboard/src/setupTests.js
@@ -3,6 +3,6 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-17-updated';
+import Adapter from 'enzyme-adapter-react-16-updated';
 
 Enzyme.configure({ adapter: new Adapter() });
